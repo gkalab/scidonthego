601b778455e4b81d131f78e04df724ba9d3ec07a
gerhard.kalab <gerhard.kalab@gmail.com>
2010-11-16 01:23:43 +0000

    optimize filter

--- src/org/scid/database/Filter.java
+++ src/org/scid/database/Filter.java
@@ -1,55 +1,49 @@
 package org.scid.database;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Filter {
-	private List<Integer> plyList = new ArrayList<Integer>();
+	int[] plyList = {};
+	int[] gameList = {};
 
 	public Filter(int[] filter) {
-		plyList = new ArrayList<Integer>();
-		for (int i = 0; i < filter.length; i++) {
-			plyList.add(filter[i]);
-		}
-	}
-
-	public List<Integer> getGameList() {
-		List<Integer> gameList = new ArrayList<Integer>();
-		int gameNo = 0;
-		for (Integer i : plyList) {
-			if (i != 0) {
-				gameList.add(gameNo);
+		if (filter != null) {
+			plyList = filter;
+			int count = 0;
+			for (int i = 0; i < plyList.length; i++) {
+				if (plyList[i] > 0) {
+					count++;
+				}
 			}
-			gameNo++;
+			gameList = new int[count];
+			int j = 0;
+			for (int i = 0; i < plyList.length; i++) {
+				if (plyList[i] > 0) {
+					gameList[j] = i;
+					j++;
+				}
+			}
 		}
-		return gameList;
 	}
 
 	public int getGameNo(int index) {
-		List<Integer> gameList = getGameList();
-		if (gameList.size() > index) {
-			return gameList.get(index);
+		if (gameList.length > index) {
+			return gameList[index];
 		}
 		return -1;
 	}
 
 	public int getSize() {
-		return getGameList().size();
+		return gameList.length;
 	}
 
 	public int getGamePly(int index) {
 		int gameNo = getGameNo(index);
-		if (plyList.size() > gameNo) {
-			return plyList.get(gameNo);
+		if (plyList.length > gameNo) {
+			return plyList[gameNo];
 		}
 		return -1;
 	}
 
 	public int[] getFilter() {
-		final int[] result = new int[this.plyList.size()];
-		for (int i = 0; i < this.plyList.size(); i++) {
-			result[i] = this.plyList.get(i).intValue();
-		}
-		return result;
+		return plyList;
 	}
 }
