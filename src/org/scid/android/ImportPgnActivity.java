fc72a5114c5af071924ee2acf4418143786a408a
gerhard.kalab <gerhard.kalab@gmail.com>
2011-02-14 18:03:34 +0000

    add pgn import; add TWIC (current issue) import

---
+++ src/org/scid/android/ImportPgnActivity.java
@@ -0,0 +1,45 @@
+package org.scid.android;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ImportPgnActivity extends Activity {
+	private ProgressDialog progressDlg;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pgnimport);
+		Intent i = getIntent();
+		String pgnFileName = i.getAction();
+		if (pgnFileName.length() != 0) {
+			TextView view = (TextView) findViewById(R.id.importResult);
+			this.progressDlg = ProgressDialog.show(view.getContext(), "Import",
+					"Importing...", true, false);
+			AsyncTask task = new ImportPgnTask().execute(this, pgnFileName,
+					progressDlg);
+		} else {
+			setResult(RESULT_OK);
+			finish();
+		}
+	}
+
+	public void onOkClick(View view) {
+		setResult(RESULT_OK);
+		finish();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// need to distroy progress dialog in case user turns device
+		if (progressDlg != null) {
+			progressDlg.dismiss();
+		}
+	}
+}
---
+++ src/org/scid/android/ImportTwicTask.java
@@ -0,0 +1,42 @@
+package org.scid.android;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
+
+public class ImportTwicTask extends AsyncTask {
+	private Activity activity;
+	private ProgressDialog progressDlg;
+
+	@Override
+	protected Object doInBackground(Object... params) {
+		String result = null;
+		this.activity = (Activity) params[0];
+		this.progressDlg = (ProgressDialog) params[1];
+		TwicDownloader downloader = new TwicDownloader();
+		File pgnFile = downloader.getCurrentTwic(Environment
+				.getExternalStorageDirectory()
+				+ File.separator + "scid");
+		if (pgnFile != null) {
+			result = pgnFile.getName();
+		}
+		return result;
+	}
+
+	@Override
+	protected void onPostExecute(Object result) {
+		progressDlg.dismiss();
+		String resultText = (String) result;
+		if (result != null) {
+			activity.setResult(activity.RESULT_OK, (new Intent())
+					.setAction(resultText));
+			activity.finish();
+		} else {
+			activity.setResult(activity.RESULT_CANCELED);
+		}
+	}
+}
