b0c6cf3a3011c4e66f26449385134fd74f6a2c8a
gerhard.kalab@gmail.com <gerhard.kalab@gmail.com@be035897-e1d4-306d-d8f8-d39898736242>
2011-02-16 21:59:27 +0000

    add error handling if downloading fails; release 1.06

--- trunk/src/org/scid/android/ImportTwicActivity.java
+++ trunk/src/org/scid/android/ImportTwicActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class ImportTwicActivity extends ListActivity {
@@ -19,13 +20,25 @@ public class ImportTwicActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		final ImportTwicActivity twicList = this;
-		this.progressDlg = ProgressDialog.show(this, "Getting information from TWIC",
-				"Downloading...", true, false);
+		this.progressDlg = ProgressDialog.show(this,
+				"Getting information from TWIC", "Downloading...", true, false);
 		new Thread(new Runnable() {
 
 			public void run() {
 				downloader = new TwicDownloader();
 				downloader.parseTwicSite();
+				if (downloader.getLinkList().isEmpty()) {
+					progressDlg.dismiss();
+					runOnUiThread(new Runnable() {
+						public void run() {
+							Toast.makeText(getApplicationContext(),
+									getText(R.string.download_error),
+									Toast.LENGTH_LONG).show();
+						}
+					});
+					setResult(RESULT_CANCELED);
+					finish();
+				}
 				runOnUiThread(new Runnable() {
 					public void run() {
 						twicList.showList();
@@ -46,9 +59,10 @@ public class ImportTwicActivity extends ListActivity {
 			public void onItemClick(AdapterView<?> parent, View view, int pos,
 					long id) {
 				TwicItem item = aa.getItem(pos);
-				progressDlg = ProgressDialog.show(ImportTwicActivity.this, "Downloading from TWIC",
-						"Downloading...", true, false);
-				AsyncTask task = new ImportTwicTask().execute(ImportTwicActivity.this, progressDlg, item.getLink());
+				progressDlg = ProgressDialog.show(ImportTwicActivity.this,
+						"Downloading from TWIC", "Downloading...", true, false);
+				AsyncTask task = new ImportTwicTask().execute(
+						ImportTwicActivity.this, progressDlg, item.getLink());
 			}
 		});
 
