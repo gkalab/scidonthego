aeede60402c00a1cf63f4a6aba9d18cf4bbf93a9
gerhard.kalab <gerhard.kalab@gmail.com>
2011-04-06 23:14:22 +0000

    improve uci interface

--- src/org/scid/android/engine/PipedProcess.java
+++ src/org/scid/android/engine/PipedProcess.java
@@ -23,6 +23,10 @@ public class PipedProcess {
 		}
 	}
 
+	public boolean isAlive() {
+		return processAlive;
+	}
+
 	/** Shut down process. */
 	public final void shutDown() {
 		if (processAlive) {
@@ -37,7 +41,7 @@ public class PipedProcess {
 
 	@Override
 	protected void finalize() throws Throwable {
-		if (process != null) {
+		if (processAlive && process != null) {
 			process.destroy();
 			Log.d("SCID", "uci process killed in finalize");
 		}
@@ -47,25 +51,37 @@ public class PipedProcess {
 	/**
 	 * Read a line from the process.
 	 * 
-	 * @param timeoutMillis
-	 *            Maximum time to wait for data
 	 * @return The line, without terminating newline characters, or empty string
 	 *         if no data available, or null if I/O error.
+	 * @throws IOException
 	 */
-	public final String readLineFromProcess(int timeoutMillis) {
-		String ret = readFromProcess(timeoutMillis);
-		if (ret == null)
-			return null;
-		if (ret.length() > 0) {
-			// Log.d("SCID", "Engine -> GUI: " + ret);
+	public final synchronized String readLineFromProcess(){
+		String ret=null;
+		try {
+			ret = readFromProcess();
+		} catch (IOException e) {
+			Log.e("SCID", "Error reading from process");
+			e.printStackTrace();
+		}
+		if (ret!=null && ret.length() > 0) {
+			//Log.d("SCID", "Engine -> GUI: " + ret);
 		}
 		return ret;
 	}
 
-	/** Write a line to the process. \n will be added automatically. */
+	/**
+	 * Write a line to the process. \n will be added automatically.
+	 * 
+	 * @throws IOException
+	 */
 	public final synchronized void writeLineToProcess(String data) {
-		// Log.d("SCID", "GUI -> Engine: " + data);
-		writeToProcess(data + "\n");
+		//Log.d("SCID", "GUI -> Engine: " + data);
+		try {
+			writeToProcess(data + "\n");
+		} catch (IOException e) {
+			Log.e("SCID", "Error writing to process: " + data, e);
+			processAlive = false;
+		}
 	}
 
 	/** Start the child process. */
@@ -82,40 +98,34 @@ public class PipedProcess {
 			reader = new BufferedReader(new InputStreamReader(stdout));
 			writer = new BufferedWriter(new OutputStreamWriter(stdin));
 			processAlive = true;
-
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Log.e("SCID", "Error initializing engine " + fileName, e);
 		}
 	}
 
 	/**
 	 * Read a line of data from the process. Return as soon as there is a full
 	 * line of data to return, or when timeoutMillis milliseconds have passed.
+	 * 
+	 * @throws IOException
 	 */
-	private final String readFromProcess(int timeoutMillis) {
+	private final String readFromProcess() throws IOException {
 		String line = null;
-		try {
-			if (reader != null) {
-				line = reader.readLine();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (processAlive && reader != null && reader.ready()) {
+			line = reader.readLine();
 		}
 		return line;
 	}
 
-	/** Write data to the process. */
-	private final void writeToProcess(String data) {
-		try {
-			if (writer != null) {
-				writer.write(data);
-				writer.flush();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/**
+	 * Write data to the process.
+	 * 
+	 * @throws IOException
+	 */
+	private final void writeToProcess(String data) throws IOException {
+		if (processAlive && writer != null) {
+			writer.write(data);
+			writer.flush();
 		}
 	}
 }
