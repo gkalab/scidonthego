79443c8ab750dfe204ec1ce11fb3cd27ee975ddd
gerhard.kalab <gerhard.kalab@gmail.com>
2011-04-07 00:20:09 +0000

    improve uci interface

--- src/org/scid/android/PgnScreenText.java
+++ src/org/scid/android/PgnScreenText.java
@@ -48,11 +48,13 @@ class PgnScreenText implements PgnToken.PgnTokenReceiver {
 
 	/**
 	 * Get spannable data of current move
-	 * @param whiteMove 
+	 * 
+	 * @param whiteMove
 	 * 
 	 * @return sb of current move
 	 */
-	public final SpannableStringBuilder getCurrentSpannableData(boolean whiteMove) {
+	public final SpannableStringBuilder getCurrentSpannableData(
+			boolean whiteMove) {
 		PgnScreenText.NodeInfo ni = nodeToCharPos.get(currNode);
 		if (ni != null) {
 			return new SpannableStringBuilder(currNode.getMoveString(whiteMove));
@@ -73,140 +75,146 @@ class PgnScreenText implements PgnToken.PgnTokenReceiver {
 	boolean paraBold = false;
 
 	private final void newLine() {
-		if (!col0) {
-			if (paraIndent > 0) {
-				int paraEnd = sb.length();
-				int indent = paraIndent * indentStep;
-				sb.setSpan(new LeadingMarginSpan.Standard(indent),
-						paraStart, paraEnd,
-						Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		synchronized (sb) {
+			if (!col0) {
+				if (paraIndent > 0) {
+					int paraEnd = sb.length();
+					int indent = paraIndent * indentStep;
+					sb.setSpan(new LeadingMarginSpan.Standard(indent),
+							paraStart, paraEnd,
+							Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+				}
+				if (paraBold) {
+					int paraEnd = sb.length();
+					sb.setSpan(new StyleSpan(Typeface.BOLD), paraStart,
+							paraEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+				}
+				sb.append('\n');
+				paraStart = sb.length();
+				paraIndent = nestLevel;
+				paraBold = false;
 			}
-			if (paraBold) {
-				int paraEnd = sb.length();
-				sb.setSpan(new StyleSpan(Typeface.BOLD), paraStart,
-						paraEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-			}
-			sb.append('\n');
-			paraStart = sb.length();
-			paraIndent = nestLevel;
-			paraBold = false;
+			col0 = true;
 		}
-		col0 = true;
 	}
 
 	boolean pendingNewLine = false;
 
 	public void processToken(Node node, int type, String token) {
-		if ((prevType == PgnToken.RIGHT_BRACKET)
-				&& (type != PgnToken.LEFT_BRACKET)) {
-			if (options.view.headers) {
-				col0 = false;
-				newLine();
-			} else {
-				sb.clear();
-				paraBold = false;
-			}
-		}
-		if (pendingNewLine) {
-			if (type != PgnToken.RIGHT_PAREN) {
-				newLine();
-				pendingNewLine = false;
-			}
-		}
-		switch (type) {
-		case PgnToken.STRING:
-			sb.append(" \"");
-			sb.append(token);
-			sb.append('"');
-			break;
-		case PgnToken.INTEGER:
-			if ((prevType != PgnToken.LEFT_PAREN)
-					&& (prevType != PgnToken.RIGHT_BRACKET) && !col0) {
-				sb.append(' ');
-			}
-			sb.append(token);
-			col0 = false;
-			break;
-		case PgnToken.PERIOD:
-			sb.append('.');
-			col0 = false;
-			break;
-		case PgnToken.ASTERISK:
-			sb.append(" *");
-			col0 = false;
-			break;
-		case PgnToken.LEFT_BRACKET:
-			sb.append('[');
-			col0 = false;
-			break;
-		case PgnToken.RIGHT_BRACKET:
-			sb.append("]\n");
-			col0 = false;
-			break;
-		case PgnToken.LEFT_PAREN:
-			nestLevel++;
-			if (col0) {
-				paraIndent++;
-			}
-			newLine();
-			sb.append('(');
-			col0 = false;
-			break;
-		case PgnToken.RIGHT_PAREN:
-			sb.append(')');
-			nestLevel--;
-			pendingNewLine = true;
-			break;
-		case PgnToken.NAG:
-			sb.append(Node.nagStr(Integer.parseInt(token)));
-			col0 = false;
-			break;
-		case PgnToken.SYMBOL: {
-			if ((prevType != PgnToken.RIGHT_BRACKET)
-					&& (prevType != PgnToken.LEFT_BRACKET) && !col0) {
-				sb.append(' ');
-			}
-			int l0 = sb.length();
-			sb.append(token);
-			int l1 = sb.length();
-			nodeToCharPos.put(node, new NodeInfo(l0, l1));
-			if (endPos < l0) {
-				endPos = l0;
-			}
-			col0 = false;
-			if (nestLevel == 0) {
-				paraBold = true;
-			}
-			break;
-		}
-		case PgnToken.COMMENT:
-			if (prevType == PgnToken.RIGHT_BRACKET) {
-			} else if (nestLevel == 0) {
-				nestLevel++;
-				newLine();
-				nestLevel--;
-			} else {
-				if ((prevType != PgnToken.LEFT_PAREN) && !col0) {
-					sb.append(' ');
+		synchronized (sb) {
+			if ((prevType == PgnToken.RIGHT_BRACKET)
+					&& (type != PgnToken.LEFT_BRACKET)) {
+				if (options.view.headers) {
+					col0 = false;
+					newLine();
+				} else {
+					sb.clear();
+					paraBold = false;
 				}
 			}
-			sb.append(token.replaceAll("[ \t\r\n]+", " ").trim());
-			col0 = false;
-			if (nestLevel == 0) {
-				newLine();
+			if (pendingNewLine) {
+				if (type != PgnToken.RIGHT_PAREN) {
+					newLine();
+					pendingNewLine = false;
+				}
 			}
-			break;
-		case PgnToken.EOF:
-			newLine();
-			upToDate = true;
-			break;
+			switch (type) {
+			case PgnToken.STRING:
+				sb.append(" \"");
+				sb.append(token);
+				sb.append('"');
+				break;
+			case PgnToken.INTEGER:
+				if ((prevType != PgnToken.LEFT_PAREN)
+						&& (prevType != PgnToken.RIGHT_BRACKET) && !col0) {
+					sb.append(' ');
+				}
+				sb.append(token);
+				col0 = false;
+				break;
+			case PgnToken.PERIOD:
+				sb.append('.');
+				col0 = false;
+				break;
+			case PgnToken.ASTERISK:
+				sb.append(" *");
+				col0 = false;
+				break;
+			case PgnToken.LEFT_BRACKET:
+				sb.append('[');
+				col0 = false;
+				break;
+			case PgnToken.RIGHT_BRACKET:
+				sb.append("]\n");
+				col0 = false;
+				break;
+			case PgnToken.LEFT_PAREN:
+				nestLevel++;
+				if (col0) {
+					paraIndent++;
+				}
+				newLine();
+				sb.append('(');
+				col0 = false;
+				break;
+			case PgnToken.RIGHT_PAREN:
+				sb.append(')');
+				nestLevel--;
+				pendingNewLine = true;
+				break;
+			case PgnToken.NAG:
+				sb.append(Node.nagStr(Integer.parseInt(token)));
+				col0 = false;
+				break;
+			case PgnToken.SYMBOL: {
+				if ((prevType != PgnToken.RIGHT_BRACKET)
+						&& (prevType != PgnToken.LEFT_BRACKET) && !col0) {
+					sb.append(' ');
+				}
+				int l0 = sb.length();
+				sb.append(token);
+				int l1 = sb.length();
+				nodeToCharPos.put(node, new NodeInfo(l0, l1));
+				if (endPos < l0) {
+					endPos = l0;
+				}
+				col0 = false;
+				if (nestLevel == 0) {
+					paraBold = true;
+				}
+				break;
+			}
+			case PgnToken.COMMENT:
+				if (prevType == PgnToken.RIGHT_BRACKET) {
+				} else if (nestLevel == 0) {
+					nestLevel++;
+					newLine();
+					nestLevel--;
+				} else {
+					if ((prevType != PgnToken.LEFT_PAREN) && !col0) {
+						sb.append(' ');
+					}
+				}
+				sb.append(token.replaceAll("[ \t\r\n]+", " ").trim());
+				col0 = false;
+				if (nestLevel == 0) {
+					newLine();
+				}
+				break;
+			case PgnToken.EOF:
+				newLine();
+				upToDate = true;
+				break;
+			}
+			prevType = type;
 		}
-		prevType = type;
 	}
 
 	@Override
 	public void clear() {
-		sb.clear();
+		synchronized (sb) {
+			sb.clear();
+		}
 		prevType = PgnToken.EOF;
 		nestLevel = 0;
 		col0 = true;
@@ -226,18 +234,20 @@ class PgnScreenText implements PgnToken.PgnTokenReceiver {
 
 	@Override
 	public void setCurrent(Node node) {
-		sb.removeSpan(bgSpan);
-		PgnScreenText.NodeInfo ni = nodeToCharPos.get(node);
-		if (ni != null) {
-			if (options.view.allMoves) {
-				int color = ColorTheme.instance().getColor(
-						ColorTheme.CURRENT_MOVE);
-				bgSpan = new BackgroundColorSpan(color);
-				sb.setSpan(bgSpan, ni.l0, ni.l1,
-						Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		synchronized (sb) {
+			sb.removeSpan(bgSpan);
+			PgnScreenText.NodeInfo ni = nodeToCharPos.get(node);
+			if (ni != null) {
+				if (options.view.allMoves) {
+					int color = ColorTheme.instance().getColor(
+							ColorTheme.CURRENT_MOVE);
+					bgSpan = new BackgroundColorSpan(color);
+					sb.setSpan(bgSpan, ni.l0, ni.l1,
+							Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+				}
+				currPos = ni.l0;
 			}
-			currPos = ni.l0;
+			currNode = node;
 		}
-		currNode = node;
 	}
 }
