74ff48fbf49eb901341aaead8e39960eb239d1c4
gerhard.kalab@gmail.com <gerhard.kalab@gmail.com@be035897-e1d4-306d-d8f8-d39898736242>
2011-04-08 08:59:09 +0000

    remove progress dialog for engine loading, add debug output to engine process startup

--- trunk/src/org/scid/android/StartEngineTask.java
+++ trunk/src/org/scid/android/StartEngineTask.java
@@ -2,29 +2,23 @@ package org.scid.android;
 
 import org.scid.android.gamelogic.ChessController;
 
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 
 public class StartEngineTask extends AsyncTask {
 	private ScidAndroidActivity activity;
-	private ProgressDialog progressDlg;
 	private ChessController ctrl;
 
 	@Override
 	protected Object doInBackground(Object... params) {
 		this.activity = (ScidAndroidActivity) params[0];
-		this.progressDlg = (ProgressDialog) params[1];
-		this.ctrl = (ChessController) params[2];
-		String engineFileName = (String) params[3];
+		this.ctrl = (ChessController) params[1];
+		String engineFileName = (String) params[2];
 		ctrl.startEngine(engineFileName);
 		return null;
 	}
 
 	@Override
 	protected void onPostExecute(Object result) {
-		if (progressDlg != null) {
-			progressDlg.dismiss();
-		}
 		activity.onFinishStartAnalysis();
 	}
 }
