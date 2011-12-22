a5cb7431429c8eeec2fa95a156088471c19f8f19
gerhard.kalab@gmail.com <gerhard.kalab@gmail.com@be035897-e1d4-306d-d8f8-d39898736242>
2011-12-22 00:09:03 +0000

    rework menu

--- trunk/src/org/scid/android/VersionHelper.java
+++ trunk/src/org/scid/android/VersionHelper.java
@@ -2,6 +2,9 @@ package org.scid.android;
 
 import android.app.Activity;
 
+/**
+ * Utility class to hide methods that are not available on SDK versions < 11
+ */
 public class VersionHelper
 {
     static void refreshActionBarMenu(Activity activity)
