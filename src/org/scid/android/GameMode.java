1c166a26ba0704e51e4bcf22b7334e4dc4362832
alserkli <alserkli@gmail.com>
2012-12-03 23:11:18 +0000

    refactor GameMode (1/3)
    We only use three modes, so let us remove the rest.

--- src/org/scid/android/GameMode.java
+++ src/org/scid/android/GameMode.java
@@ -1,80 +1,34 @@
 package org.scid.android;
 
 public class GameMode {
-	private final boolean playerWhite;
-	private final boolean playerBlack;
-	private final boolean analysisMode;
-	private final boolean studyMode;
 	private int mode;
 
-	public static final int PLAYER_WHITE = 1;
-	public static final int PLAYER_BLACK = 2;
-	public static final int TWO_PLAYERS = 3;
-	public static final int ANALYSIS = 4;
-	public static final int TWO_COMPUTERS = 5;
-	public static final int STUDY_MODE = 6;
+	public static final int REVIEW_MODE = 0;
+	public static final int ANALYSIS_MODE = 1;
+	public static final int STUDY_MODE = 2;
 
-	public GameMode(int modeNr) {
-		this.mode = modeNr;
-		switch (modeNr) {
-		case PLAYER_WHITE:
-		default:
-			playerWhite = true;
-			playerBlack = false;
-			analysisMode = false;
-			studyMode = false;
-			break;
-		case PLAYER_BLACK:
-			playerWhite = false;
-			playerBlack = true;
-			analysisMode = false;
-			studyMode = false;
-			break;
-		case TWO_PLAYERS:
-			playerWhite = true;
-			playerBlack = true;
-			analysisMode = false;
-			studyMode = false;
-			break;
-		case ANALYSIS:
-			playerWhite = true;
-			playerBlack = true;
-			analysisMode = true;
-			studyMode = false;
-			break;
-		case TWO_COMPUTERS:
-			playerWhite = false;
-			playerBlack = false;
-			analysisMode = false;
-			studyMode = false;
-			break;
-		case STUDY_MODE:
-			playerWhite = true;
-			playerBlack = true;
-			analysisMode = false;
-			studyMode = true;
-			break;
-		}
+	public GameMode(int mode) {
+		this.mode = mode;
 	}
 
 	public final boolean playerWhite() {
-		return playerWhite;
+		return true;
 	}
 
 	public final boolean playerBlack() {
-		return playerBlack;
+		return true;
 	}
 
 	public final boolean analysisMode() {
-		return analysisMode;
+		return mode == ANALYSIS_MODE;
 	}
 
 	public final boolean studyMode() {
-		return studyMode;
+		return mode == STUDY_MODE;
 	}
 
 	public final boolean humansTurn(boolean whiteMove) {
-		return (whiteMove ? playerWhite : playerBlack) || analysisMode;
+		return true;
 	}
 
 	@Override
@@ -82,15 +36,7 @@ public class GameMode {
 		if ((o == null) || (o.getClass() != this.getClass()))
 			return false;
 		GameMode other = (GameMode) o;
-		if (playerWhite != other.playerWhite)
-			return false;
-		if (playerBlack != other.playerBlack)
-			return false;
-		if (analysisMode != other.analysisMode)
-			return false;
-		if (studyMode != other.studyMode)
-			return false;
-		return true;
+		return mode == other.mode;
 	}
 
 	public int getMode() {
