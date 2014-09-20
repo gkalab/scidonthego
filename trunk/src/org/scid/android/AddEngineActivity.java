06b1d7a028aef382f9595a709ab130b553a5dda2
gerhard.kalab@gmail.com <gerhard.kalab@gmail.com@be035897-e1d4-306d-d8f8-d39898736242>
2014-09-20 19:13:19 +0000

    Release v1.46: hotfix to not display previously added engines in manage engines list

--- trunk/src/org/scid/android/AddEngineActivity.java
+++ trunk/src/org/scid/android/AddEngineActivity.java
@@ -49,15 +49,10 @@ public class AddEngineActivity extends Activity {
 				Arrays.asList(new String[] { ".sg4", ".sn4", ".si4", ".pgn",
 						".zip", ".xml" }));
 
-		// Build set of engines starting with engines already added.
-		// Once per engine configuration preferences are possible,
-		// multiple engine configurations using the same engine may be
-		// useful.
+		// Build set of engines. Add any additional engines from the external
+		// directory.
 		SortedSet<Engine> engines = Tools.findEnginesInDirectory(
-				"/data/data/org.scid.android/", ignoreExtensions);
-		// Add any additional engines from the external directory.
-		engines.addAll(Tools.findEnginesInDirectory(Tools.getScidDirectory(),
-				ignoreExtensions));
+				Tools.getScidDirectory(), ignoreExtensions);
 
 		executablesList = new ArrayList<Engine>(engines);
 		addOpenExchangeFormatEngines();
