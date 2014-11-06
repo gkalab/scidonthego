b74df46972364c6b24c70247239eda4d9873cdf7
gerhard.kalab <gerhard.kalab@gmail.com>
2014-11-06 16:10:28 +0000

    Make sure the used engine is up-to-date when using open exchange engines.

--- src/org/scid/android/engine/Engine.java
+++ src/org/scid/android/engine/Engine.java
@@ -6,13 +6,20 @@ public class Engine implements Comparable<Engine> {
 
 	private String name;
 
+	private String packageName = null;
+
+	private int versionCode = 0;
+
 	public Engine(String fileName) {
 		this.fileName = fileName;
 	}
 
-	public Engine(String name, String fileName) {
+	public Engine(String name, String fileName, String packageName,
+			int versionCode) {
 		this.name = name;
 		this.fileName = fileName;
+		this.packageName = packageName;
+		this.versionCode = versionCode;
 	}
 
 	public String getFileName() {
@@ -26,5 +33,13 @@ public class Engine implements Comparable<Engine> {
 	@Override
 	public int compareTo(Engine otherEngine) {
 		return fileName.compareTo(otherEngine.getFileName());
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public int getVersionCode() {
+		return versionCode;
 	}
 }
